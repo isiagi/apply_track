a308a943fbd615e64d114d908b76ff6e1b4f76c5
isiagi <isiagigeofrey0@gmail.com>
2023-08-16 10:46:18 +0300

    chore: modify ui

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,15 +7,15 @@ function NavComponent() {
   const { isSignedIn } = useUser();
 
   return (
-    <nav className="px-10 flex items-center justify-between h-14 bg-[#FAFAFA]">
-      <h1 className="text-lg text-[#10B981] ">Jobs Applys</h1>
+    <nav className="md:px-10 px-2 flex items-center justify-between h-16 bg-[#FAFAFA]">
+      <Link href="/"><h1 className="text-lg text-[#10B981] ">JOBS APPLY</h1></Link>
       {isSignedIn ? (
         <UserButton afterSignOutUrl="/" />
       ) : (
         <ul className="flex flex-wrap gap-10">
           <Link href="/sign-up">
             <li className="text-stone-600 text-lg">
-              <button>Sign In</button>
+              <button className="bg-[#08D1C7] text-white p-2 rounded outline-none">Sign In</button>
             </li>
           </Link>
         </ul>
--- components/antTable/Table.jsx
+++ components/antTable/Table.jsx
@@ -2,105 +2,58 @@ import React, { useState } from "react";
 import { Space, Table, Tag } from "antd";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
-const handleEdit = (id) => {
-    router.push(`/edit?id=${id}`);
-  };
-
-  const handleDelete = async (id) => {
-    try {
-      await fetch(`/api/action/${id.toString()}`, { method: "DELETE" });
-      const newData = applys.filter((res) => res._id !== id);
-
-      setApplys(newData);
-    } catch (error) {
-      return new Response(error.message, { status: 500 });
-    }
-  };
-
-const columns = [
-  {
-    title: "Company",
-    dataIndex: "company",
-    key: "company",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Position",
-    dataIndex: "position",
-    key: "position",
-  },
-  {
-    title: "Date Of Applying",
-    dataIndex: "date",
-    key: "date",
-  },
-  {
-    title: "Status",
-    key: "status",
-    dataIndex: "status",
-    render: (status) => (
-      <>
-      
-        <Tag>
-          {status.toUpperCase()}
-        </Tag>
-      </>
-      //   <span>
-      //     {tags.map((tag) => {
-      //       let color = tag.length > 5 ? "geekblue" : "green";
-      //       if (tag === "loser") {
-      //         color = "volcano";
-      //       }
-      //       return (
-      //         <Tag color={color} key={tag}>
-      //           {tag.toUpperCase()}
-      //         </Tag>
-      //       );
-      //     })}
-      //   </span>
-    ),
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <div onClick={() => handleEdit(record._id)}>
-          Edit
-          <EditOutlined /> {record.name}
-        </div>
-        <div onClick={() => handleDelete(record._id)}>
-          Delete
-          <DeleteOutlined />
-        </div>
-      </Space>
-    ),
-  },
-];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
 const App = ({ data, handleDelete, handleEdit, submitting }) => {
+  const columns = [
+    {
+      title: "Company",
+      dataIndex: "company",
+      key: "company",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Position",
+      dataIndex: "position",
+      key: "position",
+    },
+    {
+      title: "Date Of Applying",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
+      render: (status) => (
+        <div>
+          {<Tag color={`${tagColor(status)}`}>{status.toUpperCase()}</Tag>}
+        </div>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <div
+            className="text-green-400 hover:text-green-600 flex items-center gap-1 cursor-pointer"
+            onClick={() => handleEdit(record._id)}
+          >
+            <p>Edit</p>
+            <EditOutlined /> {record.name}
+          </div>
+          <div
+            className="text-red-400 hover:text-red-600 flex items-center gap-1 cursor-pointer"
+            onClick={() => handleDelete(record._id)}
+          >
+            Delete
+            <DeleteOutlined />
+          </div>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <div>
       <Table
@@ -114,3 +67,22 @@ const App = ({ data, handleDelete, handleEdit, submitting }) => {
   );
 };
 export default App;
+
+const tagColor = (tag) => {
+  let color = "";
+  switch (tag) {
+    case "Pending":
+      return (color = "geekblue");
+    case "Short-Listed":
+      return (color = "pink");
+    case "Interviewed":
+      return (color = "orange");
+    case "Got Hired":
+      return (color = "green");
+    case "Rejected":
+      return (color = "volcano");
+
+    default:
+      return (color = "purple");
+  }
+};
